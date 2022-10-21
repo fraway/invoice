16599768e8c5c43cd66fcede711cd582034788e9
fdonzello <francesco@fraway.io>
2022-10-21 11:16:30 +0200

    creato material module per esportare tutti i moduli di material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+const modules = [
+  LayoutModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatFormFieldModule,
+  MatInputModule,
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ...modules
+  ],
+  exports: modules
+})
+export class MaterialModule { }
